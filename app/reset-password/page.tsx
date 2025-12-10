273784d3ad365b6a262a21510f0de89b79f1b8d1
01skyler <karlis.pujats@gmail.com>
2025-12-10 14:36:10 -0500

    Initial commit p

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-export const fetchCache = "force-no-store";
+// export const dynamic = "force-dynamic";
+// export const revalidate = 0;
+// export const fetchCache = "force-no-store";
 
 import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
